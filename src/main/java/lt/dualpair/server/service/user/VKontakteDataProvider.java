1c9022994f190752b29506cc027f77ca4e04ffe9
p998rtu <kur.artur@gmail.com>
2016-11-22 09:30:47 +0200

    #3 vkontakte data provider will enhance user with basic data

--- src/main/java/lt/dualpair/server/service/user/VKontakteDataProvider.java
+++ src/main/java/lt/dualpair/server/service/user/VKontakteDataProvider.java
@@ -5,8 +5,14 @@ import lt.dualpair.server.domain.model.user.User;
 import org.jboss.logging.Logger;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.vkontakte.api.VKontakte;
+import org.springframework.social.vkontakte.api.VKontakteDate;
+import org.springframework.social.vkontakte.api.VKontakteProfile;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +33,43 @@ public class VKontakteDataProvider implements SocialDataProvider {
 
     @Override
     public User enhanceUser(User user) throws SocialDataException {
+        VKontakte vKontakte = vkontakteConnection.getApi();
+        VKontakteProfile vkUser = vKontakte.usersOperations().getUser("sex, bdate");
+        if (!StringUtils.isEmpty(vkUser.getFirstName())) {
+            user.setName(vkUser.getFirstName());
+        }
+        user.setEmail(vkontakteConnection.fetchUserProfile().getEmail());
+        user.setGender(resolveGender(vkUser.getGender()));
+
+        VKontakteDate birthDate = vkUser.getBirthDate();
+        if (!StringUtils.isEmpty(birthDate)) {
+            Date dateOfBirth = vkBirthDateToDate(birthDate);
+            if (dateOfBirth != null) {
+                user.setDateOfBirth(dateOfBirth);
+            }
+        }
+
+        //addPhotos(user, vKontakte);
+
+        return user;
+    }
+
+    private User.Gender resolveGender(String gender) throws SocialDataException {
+        if ("2".equals(gender)) {
+            return User.Gender.MALE;
+        } else if ("1".equals(gender)) {
+            return User.Gender.FEMALE;
+        } else {
+            logger.error("Invalid gender " + gender);
+            throw new SocialDataException("Invalid gender '" + gender +"'");
+        }
+    }
+
+    private Date vkBirthDateToDate(VKontakteDate birthDate) {
+        if (birthDate.getYear() != 0) {
+            LocalDate localDate = LocalDate.of(birthDate.getYear(), birthDate.getMonth(), birthDate.getDay());
+            return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        }
         return null;
     }
 
--- src/test/java/lt/dualpair/server/service/user/VKontakteDataProviderTest.java
+++ src/test/java/lt/dualpair/server/service/user/VKontakteDataProviderTest.java
@@ -1,13 +1,18 @@
 package lt.dualpair.server.service.user;
 
+import lt.dualpair.server.domain.model.user.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.UserProfile;
 import org.springframework.social.vkontakte.api.IUsersOperations;
 import org.springframework.social.vkontakte.api.VKontakte;
+import org.springframework.social.vkontakte.api.VKontakteDate;
 import org.springframework.social.vkontakte.api.VKontakteProfile;
 
-import static org.junit.Assert.assertEquals;
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,6 +28,7 @@ public class VKontakteDataProviderTest {
         vKontakteDataProvider = new VKontakteDataProvider(connection);
         when(connection.getApi()).thenReturn(vkontakte);
         when(vkontakte.usersOperations()).thenReturn(usersOperations);
+        when(connection.fetchUserProfile()).thenReturn(new UserProfile(null, null, null, null, "mail@mail.com", null));
     }
 
     @Test
@@ -35,7 +41,51 @@ public class VKontakteDataProviderTest {
 
     @Test
     public void enhanceUser() throws Exception {
+        VKontakteProfile vKontakteProfile = new VKontakteProfile();
+        vKontakteProfile.setFirstName("firstName");
+        vKontakteProfile.setGender("1");
+        vKontakteProfile.setBirthDate(new VKontakteDate(1, 2, 1990));
+        when(usersOperations.getUser("sex, bdate")).thenReturn(vKontakteProfile);
+        User user = vKontakteDataProvider.enhanceUser(new User());
+        assertEquals("firstName", user.getName());
+        assertEquals(new SimpleDateFormat("yyyy-MM-dd").parse("1990-02-01"), user.getDateOfBirth());
+        assertEquals(User.Gender.FEMALE, user.getGender());
+        assertEquals("mail@mail.com", user.getEmail());
+    }
 
+    @Test
+    public void enhanceUser_sex() throws Exception {
+        VKontakteProfile vKontakteProfile = new VKontakteProfile();
+        vKontakteProfile.setGender("1");
+        when(usersOperations.getUser("sex, bdate")).thenReturn(vKontakteProfile);
+        User user = vKontakteDataProvider.enhanceUser(new User());
+        assertEquals(User.Gender.FEMALE, user.getGender());
+
+        vKontakteProfile.setGender("2");
+        user = vKontakteDataProvider.enhanceUser(new User());
+        assertEquals(User.Gender.MALE, user.getGender());
+
+        vKontakteProfile.setGender(null);
+        try {
+            user = vKontakteDataProvider.enhanceUser(new User());
+            fail();
+        } catch (SocialDataException sde) {
+            assertTrue(sde.getMessage().contains("Invalid gender"));
+        }
+    }
+
+    @Test
+    public void enhanceUser_bDate() throws Exception {
+        VKontakteProfile vKontakteProfile = new VKontakteProfile();
+        vKontakteProfile.setGender("1");
+        vKontakteProfile.setBirthDate(new VKontakteDate(1, 2, 1990));
+        when(usersOperations.getUser("sex, bdate")).thenReturn(vKontakteProfile);
+        User user = vKontakteDataProvider.enhanceUser(new User());
+        assertEquals(new SimpleDateFormat("yyyy-MM-dd").parse("1990-02-01"), user.getDateOfBirth());
+
+        vKontakteProfile.setBirthDate(new VKontakteDate(1, 2, 0));
+        user = vKontakteDataProvider.enhanceUser(new User());
+        assertNull(user.getDateOfBirth());
     }
 
     @Test
