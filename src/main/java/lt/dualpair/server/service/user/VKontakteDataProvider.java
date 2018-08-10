be8514de357f9fe17ad2db675d8556b2aca2180a
artur <kur.artur@gmail.com>
2018-08-10 18:20:09 +0200

    Use VK API in data provider.

--- src/main/java/lt/dualpair/server/service/user/VKontakteDataProvider.java
+++ src/main/java/lt/dualpair/server/service/user/VKontakteDataProvider.java
@@ -6,6 +6,9 @@ import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.base.Sex;
+import com.vk.api.sdk.objects.users.UserXtrCounters;
+import com.vk.api.sdk.queries.users.UserField;
 import lt.dualpair.core.photo.Photo;
 import lt.dualpair.core.user.Gender;
 import lt.dualpair.core.user.User;
@@ -13,27 +16,23 @@ import org.jboss.logging.Logger;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionData;
 import org.springframework.social.vkontakte.api.VKontakte;
-import org.springframework.social.vkontakte.api.VKontakteDate;
-import org.springframework.social.vkontakte.api.VKontakteProfile;
 import org.springframework.util.StringUtils;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class VKontakteDataProvider implements SocialDataProvider {
 
     private static final Logger logger = Logger.getLogger(VKontakteDataProvider.class.getName());
 
-    private Connection<? extends VKontakte> vkontakteConnection;
-
     private VkApiClient vkApiClient = createVpApiClient();
-
     private UserActor userActor;
 
     public VKontakteDataProvider(Connection<? extends VKontakte> vkontakteConnection) {
-        this.vkontakteConnection = vkontakteConnection;
         ConnectionData connectionData = vkontakteConnection.createData();
         userActor = new UserActor(Integer.valueOf(connectionData.getProviderUserId()), connectionData.getAccessToken());
     }
@@ -50,23 +49,26 @@ public class VKontakteDataProvider implements SocialDataProvider {
 
     @Override
     public User enhanceUser(User user) throws SocialDataException {
-        VKontakte vKontakte = vkontakteConnection.getApi();
-        VKontakteProfile vkUser = vKontakte.usersOperations().getUser("sex, bdate");
-        if (!StringUtils.isEmpty(vkUser.getFirstName())) {
-            user.setName(vkUser.getFirstName());
-        }
-        user.setEmail(vkontakteConnection.fetchUserProfile().getEmail());
-        user.setGender(resolveGender(vkUser.getGender()));
-
-        VKontakteDate birthDate = vkUser.getBirthDate();
-        if (!StringUtils.isEmpty(birthDate)) {
-            Date dateOfBirth = vkBirthDateToDate(birthDate);
-            if (dateOfBirth != null) {
-                user.setDateOfBirth(dateOfBirth);
-            }
-        }
-
         try {
+            List<UserXtrCounters> list = vkApiClient.users()
+                    .get(userActor)
+                    .fields(UserField.SEX, UserField.BDATE)
+                    .execute();
+            if (!list.isEmpty()) {
+                UserXtrCounters userXtrCounters = list.get(0);
+                if (!StringUtils.isEmpty(userXtrCounters.getFirstName())) {
+                    user.setName(userXtrCounters.getFirstName());
+                }
+                // TODO email user.setEmail(null);
+                user.setGender(resolveGender(userXtrCounters.getSex()));
+                String birthDate = userXtrCounters.getBdate();
+                if (!StringUtils.isEmpty(birthDate)) {
+                    Date dateOfBirth = vkBirthDateToDate(birthDate);
+                    if (dateOfBirth != null) {
+                        user.setDateOfBirth(dateOfBirth);
+                    }
+                }
+            }
             user.setPhotos(vkApiClient.photos().get(userActor)
                         .albumId("profile")
                         .count(User.MAX_NUMBER_OF_PHOTOS)
@@ -87,23 +89,24 @@ public class VKontakteDataProvider implements SocialDataProvider {
         return user;
     }
 
-    private Gender resolveGender(String gender) throws SocialDataException {
-        if ("M".equals(gender)) {
+    private Gender resolveGender(Sex gender) {
+        if (gender == Sex.MALE) {
             return Gender.MALE;
-        } else if ("W".equals(gender)) {
+        } else if (gender == Sex.FEMALE) {
             return Gender.FEMALE;
-        } else {
-            logger.error("Invalid gender " + gender);
-            throw new SocialDataException("Invalid gender '" + gender +"'");
-        }
-    }
-
-    private Date vkBirthDateToDate(VKontakteDate birthDate) {
-        if (birthDate.getYear() != 0) {
-            LocalDate localDate = LocalDate.of(birthDate.getYear(), birthDate.getMonth(), birthDate.getDay());
-            return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
         }
         return null;
+    }
+
+    private Date vkBirthDateToDate(String birthDate) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d.M.yyyy", Locale.ENGLISH);
+            simpleDateFormat.setLenient(false);
+            return simpleDateFormat.parse(birthDate);
+        } catch (ParseException e) {
+            logger.error(e);
+            return null;
+        }
     }
 
 }
--- src/test/java/lt/dualpair/server/service/user/VKontakteDataProviderTest.java
+++ src/test/java/lt/dualpair/server/service/user/VKontakteDataProviderTest.java
@@ -1,10 +1,15 @@
 package lt.dualpair.server.service.user;
 
 import com.vk.api.sdk.actions.Photos;
+import com.vk.api.sdk.actions.Users;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.objects.base.Sex;
 import com.vk.api.sdk.objects.photos.responses.GetResponse;
+import com.vk.api.sdk.objects.users.UserXtrCounters;
 import com.vk.api.sdk.queries.photos.PhotosGetQuery;
+import com.vk.api.sdk.queries.users.UserField;
+import com.vk.api.sdk.queries.users.UsersGetQuery;
 import lt.dualpair.core.photo.Photo;
 import lt.dualpair.core.user.Gender;
 import lt.dualpair.core.user.User;
@@ -13,16 +18,13 @@ import org.junit.Test;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionData;
 import org.springframework.social.connect.UserProfile;
-import org.springframework.social.vkontakte.api.IUsersOperations;
-import org.springframework.social.vkontakte.api.VKontakte;
-import org.springframework.social.vkontakte.api.VKontakteDate;
-import org.springframework.social.vkontakte.api.VKontakteProfile;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -30,18 +32,16 @@ import static org.mockito.Mockito.when;
 public class VKontakteDataProviderTest {
 
     private VKontakteDataProvider vKontakteDataProvider;
-    private Connection<VKontakte> connection = mock(Connection.class);
-    private VKontakte vkontakte = mock(VKontakte.class);
-    private IUsersOperations usersOperations = mock(IUsersOperations.class);
+    private Connection connection = mock(Connection.class);
     private VkApiClient vkApiClient = mock(VkApiClient.class);
     private Photos photos = mock(Photos.class);
+    private Users users = mock(Users.class);
 
     @Before
     public void setUp() throws Exception {
-        when(connection.getApi()).thenReturn(vkontakte);
         when(connection.createData()).thenReturn(new ConnectionData("vkontakte", "1", null, null, null, null, null, null, null));
-        when(vkontakte.usersOperations()).thenReturn(usersOperations);
         when(vkApiClient.photos()).thenReturn(photos);
+        when(vkApiClient.users()).thenReturn(users);
         when(connection.fetchUserProfile()).thenReturn(new UserProfile(null, null, null, null, "mail@mail.com", null));
         vKontakteDataProvider = new VKontakteDataProvider(connection) {
             @Override
@@ -49,6 +49,16 @@ public class VKontakteDataProviderTest {
                 return vkApiClient;
             }
         };
+
+        List<com.vk.api.sdk.objects.photos.Photo> vkPhotos = Arrays.asList(mockVkPhoto(10, "src"));
+        PhotosGetQuery query = mock(PhotosGetQuery.class);
+        GetResponse response = mock(GetResponse.class);
+        when(query.execute()).thenReturn(response);
+        when(query.albumId(any(String.class))).thenReturn(query);
+        when(query.count(any(Integer.class))).thenReturn(query);
+        when(query.rev(true)).thenReturn(query);
+        when(photos.get(any(UserActor.class))).thenReturn(query);
+        when(response.getItems()).thenReturn(vkPhotos);
     }
 
     @Test
@@ -58,28 +68,16 @@ public class VKontakteDataProviderTest {
 
     @Test
     public void enhanceUser() throws Exception {
-        VKontakteProfile vKontakteProfile = new VKontakteProfile();
-        vKontakteProfile.setId(10L);
-        vKontakteProfile.setFirstName("firstName");
-        vKontakteProfile.setGender("W");
-        vKontakteProfile.setBirthDate(new VKontakteDate(1, 2, 1990));
-        when(usersOperations.getUser("sex, bdate")).thenReturn(vKontakteProfile);
-
-        List<com.vk.api.sdk.objects.photos.Photo> vkPhotos = Arrays.asList(mockVkPhoto(10, "src"));
-        PhotosGetQuery query = mock(PhotosGetQuery.class);
-        GetResponse response = mock(GetResponse.class);
-        when(query.execute()).thenReturn(response);
-        when(query.albumId(any(String.class))).thenReturn(query);
-        when(query.count(any(Integer.class))).thenReturn(query);
-        when(query.rev(true)).thenReturn(query);
-        when(photos.get(any(UserActor.class))).thenReturn(query);
-        when(response.getItems()).thenReturn(vkPhotos);
+        UserXtrCounters vkUser = mockVkUser("firstName", Sex.FEMALE, "1.2.1990");
+        UsersGetQuery usersQueryMock = mock(UsersGetQuery.class);
+        when(users.get(any(UserActor.class))).thenReturn(usersQueryMock);
+        when(usersQueryMock.fields(UserField.SEX, UserField.BDATE)).thenReturn(usersQueryMock);
+        when(usersQueryMock.execute()).thenReturn(Arrays.asList(vkUser));
 
         User user = vKontakteDataProvider.enhanceUser(new User());
         assertEquals("firstName", user.getName());
         assertEquals(new SimpleDateFormat("yyyy-MM-dd").parse("1990-02-01"), user.getDateOfBirth());
         assertEquals(Gender.FEMALE, user.getGender());
-        assertEquals("mail@mail.com", user.getEmail());
         Photo photo = user.getPhotos().get(0);
         assertEquals("src", photo.getSourceLink());
         assertEquals(user, photo.getUser());
@@ -87,59 +85,36 @@ public class VKontakteDataProviderTest {
 
     @Test
     public void enhanceUser_sex() throws Exception {
-        VKontakteProfile vKontakteProfile = new VKontakteProfile();
-        vKontakteProfile.setId(10L);
-        vKontakteProfile.setGender("W");
-        when(usersOperations.getUser("sex, bdate")).thenReturn(vKontakteProfile);
-
-        List<com.vk.api.sdk.objects.photos.Photo> vkPhotos = Arrays.asList(mockVkPhoto(10, "src"));
-        PhotosGetQuery query = mock(PhotosGetQuery.class);
-        GetResponse response = mock(GetResponse.class);
-        when(query.execute()).thenReturn(response);
-        when(query.albumId(any(String.class))).thenReturn(query);
-        when(query.count(any(Integer.class))).thenReturn(query);
-        when(query.rev(true)).thenReturn(query);
-        when(photos.get(any(UserActor.class))).thenReturn(query);
-        when(response.getItems()).thenReturn(vkPhotos);
+        UserXtrCounters vkUser = mockVkUser("firstName", Sex.FEMALE, "1.2.1990");
+        UsersGetQuery usersQueryMock = mock(UsersGetQuery.class);
+        when(users.get(any(UserActor.class))).thenReturn(usersQueryMock);
+        when(usersQueryMock.fields(UserField.SEX, UserField.BDATE)).thenReturn(usersQueryMock);
+        when(usersQueryMock.execute()).thenReturn(Arrays.asList(vkUser));
 
         User user = vKontakteDataProvider.enhanceUser(new User());
         assertEquals(Gender.FEMALE, user.getGender());
 
-        vKontakteProfile.setGender("M");
+        when(vkUser.getSex()).thenReturn(Sex.MALE);
         user = vKontakteDataProvider.enhanceUser(new User());
         assertEquals(Gender.MALE, user.getGender());
 
-        vKontakteProfile.setGender(null);
-        try {
-            user = vKontakteDataProvider.enhanceUser(new User());
-            fail();
-        } catch (SocialDataException sde) {
-            assertTrue(sde.getMessage().contains("Invalid gender"));
-        }
+        when(vkUser.getSex()).thenReturn(Sex.UNKNOWN);
+        user = vKontakteDataProvider.enhanceUser(new User());
+        assertNull(user.getGender());
     }
 
     @Test
     public void enhanceUser_bDate() throws Exception {
-        VKontakteProfile vKontakteProfile = new VKontakteProfile();
-        vKontakteProfile.setId(10L);
-        vKontakteProfile.setGender("W");
-        vKontakteProfile.setBirthDate(new VKontakteDate(1, 2, 1990));
-        when(usersOperations.getUser("sex, bdate")).thenReturn(vKontakteProfile);
-
-        List<com.vk.api.sdk.objects.photos.Photo> vkPhotos = Arrays.asList(mockVkPhoto(10, "src"));
-        PhotosGetQuery query = mock(PhotosGetQuery.class);
-        GetResponse response = mock(GetResponse.class);
-        when(query.execute()).thenReturn(response);
-        when(query.albumId(any(String.class))).thenReturn(query);
-        when(query.count(any(Integer.class))).thenReturn(query);
-        when(query.rev(true)).thenReturn(query);
-        when(photos.get(any(UserActor.class))).thenReturn(query);
-        when(response.getItems()).thenReturn(vkPhotos);
+        UserXtrCounters vkUser = mockVkUser("firstName", Sex.FEMALE, "1.2.1990");
+        UsersGetQuery usersQueryMock = mock(UsersGetQuery.class);
+        when(users.get(any(UserActor.class))).thenReturn(usersQueryMock);
+        when(usersQueryMock.fields(UserField.SEX, UserField.BDATE)).thenReturn(usersQueryMock);
+        when(usersQueryMock.execute()).thenReturn(Arrays.asList(vkUser));
 
         User user = vKontakteDataProvider.enhanceUser(new User());
         assertEquals(new SimpleDateFormat("yyyy-MM-dd").parse("1990-02-01"), user.getDateOfBirth());
 
-        vKontakteProfile.setBirthDate(new VKontakteDate(1, 2, 0));
+        when(vkUser.getBdate()).thenReturn("1.2.0");
         user = vKontakteDataProvider.enhanceUser(new User());
         assertNull(user.getDateOfBirth());
     }
@@ -156,6 +131,14 @@ public class VKontakteDataProviderTest {
         when(vkPhoto.getPhoto604()).thenReturn(src);
         when(vkPhoto.getId()).thenReturn(id);
         return vkPhoto;
+    }
+
+    private UserXtrCounters mockVkUser(String firstName, Sex sex, String bdate) {
+        UserXtrCounters user = mock(UserXtrCounters.class);
+        when(user.getFirstName()).thenReturn(firstName);
+        when(user.getSex()).thenReturn(sex);
+        when(user.getBdate()).thenReturn(bdate);
+        return user;
     }
 
 }
